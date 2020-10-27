35911398808b2015b29c8432d51aa2225c0deda1
Gurnain Singh <gurnain@Gurnains-MacBook-Pro.local>
2020-10-26 21:22:08 -0400

    Adding boilerplate nodejs code

---
+++ backend/app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
+const cors = require("cors");
+require("dotenv/config");
+
+// Middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+
+// Get back all the posts
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Add a post
+app.post("/posts", (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+// Get a specific post
+app.get("/posts/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Delete a Post
+app.delete("/posts/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Update a Post
+app.patch("/posts/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB!")
+);
+
+// Run on this port
+app.listen(3000);
