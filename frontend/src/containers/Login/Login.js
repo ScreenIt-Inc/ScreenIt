da879556d4d6de9192ece986b6fec451e69c8512
andyh98 <hameea1@mcmaster.ca>
2021-01-09 10:12:22 -0500

    Login Initial Draft

--- frontend/src/containers/Login/Login.js
+++ frontend/src/containers/Login/Login.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Btn} from '../../components/Controls/Button/Button';
+import { Grid , TextField , InputAdornment, Button, IconButton} from "@material-ui/core"
+import Logo from "../../assets/images/ScreenitOriginal.png"
+import SidebarLogo from "../../assets/images/screeningTemp.jpg"
 import History from '../../routes/History';
+import { AccountCircle, LockRounded, Visibility } from '@material-ui/icons'
 class Login extends React.Component {
 
     // this method is only to trigger route guards , remove and use your own logic
@@ -11,9 +15,63 @@ class Login extends React.Component {
 
     render(){
         return(
-            <div className="container my-5">
-                <h1>Login Page</h1>
-                <Btn text='Login' handleClick={this.handleLogin}/>
+            <div>
+                <Grid container style={{ minHeight: '100vh'}}>
+                    <Grid item xs={12} sm={6}>
+                    <img 
+                      src={SidebarLogo} 
+                      style={{ width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden'}}
+                      alt="ScreenIt"
+                     />
+                    </Grid>
+                    <Grid container item xs={12} sm={6} alignItems="center" direction="column" style={{padding: 10}}>
+                        <div />
+                        <div style={{ display: "flex", flexDirection: "column", maxWidth: 400, minWidth: 300 }}>
+                            <Grid container jutify="center">
+                                        <img 
+                                        src={Logo} 
+                                        width={300} 
+                                        alt="logo" 
+                                        />
+                            </Grid>
+                            <TextField label="Username" margin="normal" 
+                            InputProps={{ 
+                                startAdornment:(
+                                 <InputAdornment position="start">
+                                     <AccountCircle />
+                                </InputAdornment>
+                                ), }}
+                            />
+                            <TextField type="password" label="Password" margin="normal"
+                             InputProps={{ 
+                                startAdornment:(
+                                 <InputAdornment position="start">
+                                     <LockRounded />
+                                </InputAdornment>
+                                ),
+                                // endAdornment:(
+                                //     <InputAdornment position="end" >
+                                //         <IconButton outline>
+                                //             <Visibility fontSize='small' />
+                                //         </IconButton>
+                                //    </InputAdornment>
+                                //    )
+                            }}
+                            />
+                            <div style={{ height: 20}} />
+                            <Button color="primary" variant="contained" onClick={this.handleLogin}>
+                                Log In
+                            </Button>
+                            <div style={{ height: 20}} />
+                            <Button>Register</Button>
+                        </div>
+                        <Grid container justify="center">
+                            <Grid item>
+                                <Button>Forgot Passowrd?</Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
