362d81a7437155fed0406c27f9c61a9e552b6a9f
Tasmiha Hassan <htasmiha@gmail.com>
2021-01-20 10:57:59 -0500

    You should retrieve logger from Container instance instead of from Loader file

--- backend/src/api/routes/form.ts
+++ backend/src/api/routes/form.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import Logger from '../../loaders/logger';
-
 const route = Router()
-
+import { Logger } from 'winston';
+import { Container } from 'typedi';
 
 export default (app: Router) => {
 	app.use('/form', route);
+	const Logger : Logger = Container.get('logger');
 
   	route.post("/testurl", function(req: Request, res: Response, next: NextFunction){
 		    console.log(req.body);
