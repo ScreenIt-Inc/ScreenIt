2d96161be1cbe0809835d2a75a842e696d83ae67
Tasmiha Hassan <htasmiha@gmail.com>
2021-01-20 19:57:08 -0500

    Cleaned up Login Page

--- frontend/src/containers/Login/Login.js
+++ frontend/src/containers/Login/Login.js
@@ -1,81 +1,136 @@
-import React from 'react';
-import {Btn} from '../../components/Controls/Button/Button';
-import { Grid , TextField , InputAdornment, Button, IconButton} from "@material-ui/core"
-import Logo from "../../assets/images/ScreenitOriginal.png"
-import SidebarLogo from "../../assets/images/screeningTemp.jpg"
-import History from '../../routes/History';
-import { AccountCircle, LockRounded, Visibility } from '@material-ui/icons'
-class Login extends React.Component {
+import React, { useState } from "react";
+import {
+  Grid,
+  TextField,
+  InputAdornment,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+import Logo from "../../assets/images/ScreenitOriginal.png";
+import SidebarLogo from "../../assets/images/sidebarlogo.png";
+import History from "../../routes/History";
+import {
+  AccountCircle,
+  LockRounded,
+  Visibility,
+  VisibilityOff,
+} from "@material-ui/icons";
+export default function Login(props) {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+  // this method is only to trigger route guards , remove and use your own logic
+  const handleLogin = () => {
+    localStorage.setItem("token", "token");
+    History.push("/");
+  };
 
-    // this method is only to trigger route guards , remove and use your own logic
-    handleLogin = () => {
-        localStorage.setItem('token','token');
-        History.push('/')
-    }
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
-    render(){
-        return(
-            <div>
-                <Grid container style={{ minHeight: '100vh'}}>
-                    <Grid item xs={12} sm={6}>
-                    <img 
-                      src={SidebarLogo} 
-                      style={{ width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden'}}
-                      alt="ScreenIt"
-                     />
-                    </Grid>
-                    <Grid container item xs={12} sm={6} alignItems="center" direction="column" style={{padding: 10}}>
-                        <div />
-                        <div style={{ display: "flex", flexDirection: "column", maxWidth: 400, minWidth: 300 }}>
-                            <Grid container jutify="center">
-                                        <img 
-                                        src={Logo} 
-                                        width={300} 
-                                        alt="logo" 
-                                        />
-                            </Grid>
-                            <TextField label="Username" margin="normal" 
-                            InputProps={{ 
-                                startAdornment:(
-                                 <InputAdornment position="start">
-                                     <AccountCircle />
-                                </InputAdornment>
-                                ), }}
-                            />
-                            <TextField type="password" label="Password" margin="normal"
-                             InputProps={{ 
-                                startAdornment:(
-                                 <InputAdornment position="start">
-                                     <LockRounded />
-                                </InputAdornment>
-                                ),
-                                // endAdornment:(
-                                //     <InputAdornment position="end" >
-                                //         <IconButton outline>
-                                //             <Visibility fontSize='small' />
-                                //         </IconButton>
-                                //    </InputAdornment>
-                                //    )
-                            }}
-                            />
-                            <div style={{ height: 20}} />
-                            <Button color="primary" variant="contained" onClick={this.handleLogin}>
-                                Log In
-                            </Button>
-                            <div style={{ height: 20}} />
-                            <Button>Register</Button>
-                        </div>
-                        <Grid container justify="center">
-                            <Grid item>
-                                <Button>Forgot Passowrd?</Button>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </div>
-        )
-    }
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <Grid container style={{ minHeight: "100vh" }}>
+        <Grid item xs={12} sm={6}>
+          <img
+            src={SidebarLogo}
+            style={{
+              width: "100%",
+              height: "100vh",
+              objectFit: "cover",
+              overflow: "hidden",
+            }}
+            alt="ScreenIt"
+          />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          alignItems="center"
+          direction="column"
+          style={{ padding: 10 }}
+        >
+          <div />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: 400,
+              minWidth: 300,
+            }}
+          >
+            <Grid container justify="center">
+              <img src={Logo} width={400} alt="logo" />
+            </Grid>
+            <TextField
+              label="Email"
+              margin="normal"
+              value={values.email}
+              onChange={handleChange("email")}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              type={values.showPassword ? "text" : "password"}
+              label="Password"
+              margin="normal"
+              value={values.password}
+              onChange={handleChange("password")}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockRounded />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <div style={{ height: 20 }} />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => handleLogin()}
+            >
+              Log In
+            </Button>
+            <div style={{ height: 20 }} />
+            <Button>Register</Button>
+          </div>
+          <Grid container justify="center">
+            <Grid item>
+              <Button>Forgot Password</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-
-export default Login;
--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -3,14 +3,13 @@ import { Route, Redirect } from "react-router-dom";
 import Auth from "./Auth";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-
-  console.log(Auth.isAuth())
+  console.log("Authentication", Auth.isAuth());
   return (
     // Show the component only when the user is logged in
     // Otherwise, redirect the user to /signin page
     <Route
       {...rest}
-      render={props =>
+      render={(props) =>
         Auth.isAuth() ? <Component {...props} /> : <Redirect to={`/login`} />
       }
     />
