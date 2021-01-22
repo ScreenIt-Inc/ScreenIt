0532c2eb829cd872cab4ce20442b7613d5ede2b3
maxmerc <maxmercer36@gmail.com>
2021-01-21 20:28:24 -0500

    added contact tracing front end v1.

---
+++ frontend/src/components/Table/CustomersTable.js
@@ -0,0 +1,110 @@
+// Boilerplate for the orders table found on every page.
+import * as React from "react";
+import { useState } from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Checkbox from "@material-ui/core/Checkbox";
+import CircleCheckedFilled from "@material-ui/icons/CheckCircle";
+import CircleUnchecked from "@material-ui/icons/RadioButtonUnchecked";
+import Button from "@material-ui/core/Button";
+import Title from "./Title";
+import { useSnackbar } from "notistack";
+import {useSelector, useDispatch} from 'react-redux'
+
+const useStyles = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+  button: {
+    backgroundColor: theme.palette.primary.light
+  }
+}));
+
+function createData(id, lastName, firstName, contactNumber, emailAddress){//}), entryDate, exitDate) {
+  return { id, lastName, firstName, contactNumber, emailAddress }//, entryDate, exitDate };
+}
+
+const rows = [
+  createData( 1, 'Snow', 'Jon', '123-456-7890', "name@email.com"),//, new Date(2020, 5, 5, 5, 20, 0), new Date(2020, 5, 5, 5, 40, 0) ),
+  createData( 2, 'Lane', 'Carrie', '123-456-7890', "name@email.com"),//, new Date(2020, 5, 5, 5, 43, 0), new Date(2020, 5, 5, 6, 0, 0) ),
+  createData( 3, 'Lane', 'Jaime', '123-456-7890', "name@email.com")//, new Date(2020, 5, 5, 5, 30, 0), new Date(2020, 5, 5, 5, 45, 0) )
+];
+
+export default function CustomersTable(props) {
+  const category = useSelector(state => state.table.category)
+  const [viewAll, setViewAll] = useState(false);
+  const [paid] = useState(rows);
+  function preventDefault(event) {
+    setViewAll(!viewAll)
+    event.preventDefault();
+  }
+  const maxEvents = 10;
+
+  const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const handlePaid = (message) => {
+    enqueueSnackbar(message, { variant: "default" });
+  }
+  const date = new Date()
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          justifyContent: "space-between",
+          alignContent: "space-between",
+          display: "flex",
+        }}
+      >
+        <Title>Customers</Title>
+        <div className={classes.seeMore}>
+          <Link color="primary" href="#" onClick={preventDefault}>
+            View All
+          </Link>
+        </div>
+      </div>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell><b>First Name</b></TableCell>
+            <TableCell><b>Last Name</b></TableCell>
+            <TableCell><b>Contact Number</b></TableCell>
+            <TableCell><b>Email Address</b></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {paid.map((row, i) => {
+            return ((i < maxEvents || viewAll) && <TableRow key={row.id}>
+              <TableCell>
+                <Checkbox
+                  icon={<CircleUnchecked />}
+                  checkedIcon={<CircleCheckedFilled />}
+                  onClick={() => {
+                    console.log('checked')
+                  }}
+                />
+              </TableCell>
+              <TableCell>{row.lastName}</TableCell>
+              <TableCell>{row.firstName}</TableCell>
+              <TableCell>{row.contactNumber}</TableCell>
+              <TableCell>{row.emailAddress}</TableCell>
+            </TableRow>)
+          })}
+        </TableBody>
+      </Table>
+    </React.Fragment>
+  );
+}
+/*
+{ id: 4, lastName: 'Stark', firstName: 'Aria', contactNumber:'123-456-7890', emailAddress: "name@email.com", entryDate: new Date(2020, 5, 5, 5, 0, 0), exitDate: new Date(2020, 5, 5, 5, 15, 0) ),
+{ id: 5, lastName: 'Trails', firstName: 'Dan', contactNumber:'123-456-7890', emailAddress: "name@email.com", entryDate: new Date(2020, 5, 5, 5, 10, 0), exitDate: new Date(2020, 5, 5, 5, 30, 0) ),
+{ id: 6, lastName: 'Mercer', firstName: 'Max', contactNumber:'123-456-7890', emailAddress: "name@email.com", entryDate: new Date(2020, 10, 17, 3, 24, 0), exitDate: new Date(2020, 10, 17, 3, 26, 0) ),
+{ id: 7, lastName: 'Clifford', firstName: 'Fiona', contactNumber:'123-456-7890', emailAddress: "name@email.com", entryDate: new Date(2020, 5, 5, 5, 10, 0), exitDate: new Date(2020, 5, 5, 5, 45, 0) ),
+{ id: 8, lastName: 'Arrows', firstName: 'Ross', contactNumber:'123-456-7890', emailAddress: "name@email.com", entryDate: new Date(2020, 10, 17, 3, 23, 0), exitDate: new Date(2020, 10, 17, 3, 25, 0) ),
+{ id: 9, lastName: 'Roxie', firstName: 'Harvey', contactNumber:'123-456-7890', emailAddress: "name@email.com", entryDate: new Date(2020, 5, 5, 5, 25, 0), exitDate: new Date(2020, 5, 5, 5, 35, 0) )
+*/
--- frontend/src/containers/ContactTracing/ContactTracing.js
+++ frontend/src/containers/ContactTracing/ContactTracing.js
@@ -1,13 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+// import PaperButton from '../../components/Controls/Button/PaperButton'
+import ButtonGroup from '../../components/Controls/Button/ButtonGroup'
+import CustomersTable from '../../components/Table/CustomersTable'
+import PossibleContactsTable from '../../components/Table/PossibleContactsTable'
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
+import Paper from '@material-ui/core/Paper';
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        '& > *': {
+          margin: theme.spacing(1),
+          width: theme.spacing(16),
+          height: theme.spacing(16),
+        },
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: "flex",
+        overflow: "auto",
+        flexDirection: "column",
+    },
+    // fixedHeight: {
+    //     height: 500,
+    // }
+}));
 
 export default function ContactTracing(props) {
-    return(
-        <div className="container my-5">
-            <p>Contact Tracing</p>
-        </div>
-    )   
+  const classes = useStyles();
+  const [buttons] = useState([
+      {quantity: 128, category: "Contact Trace", icon: PeopleAltIcon}
+  ]);
+
+  return(
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={10}>
+          <Paper className={classes.paper}>
+              <CustomersTable />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={10}>
+          <Button
+            variant="contained"
+            classes={classes.button}
+            color="secondary"
+            style={{ borderRadius: 5 }}
+          >
+            <span style={{color: "white"}} >Contact Trace</span>
+          </Button>
+        </Grid>
+
+        <Grid item xs={10}>
+            <Paper className={classes.paper}>
+                <PossibleContactsTable />
+            </Paper>
+        </Grid>
+      </Grid>
+  )
 }
+/*
+<Grid item xs={12}>
+    <ButtonGroup buttons={buttons} page="Queue"/>
+</Grid>
+    */
 
 // const mapStateToProps = (state) => {
 //     return {
