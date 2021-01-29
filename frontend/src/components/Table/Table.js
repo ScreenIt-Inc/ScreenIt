8896ade150c144d62b2f4ea60de86ab30fd92eec
andyh98 <hameea1@mcmaster.ca>
2021-01-28 23:31:51 -0500

    Notify button triggers Twilio API call

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -14,7 +14,10 @@ import CircleUnchecked from "@material-ui/icons/RadioButtonUnchecked";
 import Button from "@material-ui/core/Button";
 import Title from "./Title";
 import { dispatchSnackbarSuccess } from "../../utils/Shared";
+import { dispatchSnackbarError } from "../../utils/Shared";
 import { useSelector } from "react-redux";
+import { axiosInstance } from "../../network/apis";
+import Auth from "../../utils/Auth";
 
 const useStyles = makeStyles((theme) => ({
   seeMore: {
@@ -25,12 +28,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function createData(id, name, amount, item, date) {
-  return { id, name, amount, item, date };
+function createData(id, name, number, group, date) {
+  return { id, name, number, group, date };
 }
 
 const rows = [
-  createData(0, "Justeen Randev", "416-342-5436", "2", "78", 312.44),
+  createData(0, "Justeen Randev", "+15197604419", "2", "78", 312.44),
+  createData(7, "Andy Hameed", "+16479276093", "9", "78", 549.44),
   createData(1, "Gurnain Saini", "416-342-5436", "5", "75", 866.99),
   createData(2, "Tasmiha Hassan", "416-342-5436", "4", "98", 100.81),
   createData(3, "Beyonce Knowles", "416-342-5436", "1", "87", 654.39),
@@ -55,8 +59,26 @@ export default function TableQ(props) {
   const maxEvents = 10;
 
   const classes = useStyles();
-  const handlePaid = (message) => {
-    dispatchSnackbarSuccess(message);
+  const handlePaid = async (row) => {
+    dispatchSnackbarSuccess("Notification sent to " + row.name);
+    const token = Auth.isAuth()
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+  };
+    const requestOptions = {
+      phoneNumber: row.number,
+      name: row.name
+    };
+    await axiosInstance
+      .post("/notify/sendText", requestOptions, config)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        // console.log(error.response.data.errors.message);
+        console.log(error);
+        dispatchSnackbarError(error.response.data);
+      });
   };
   const date = new Date();
   return (
@@ -112,8 +134,8 @@ export default function TableQ(props) {
                     />
                   </TableCell>
                   <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.amount}</TableCell>
-                  <TableCell>{row.item}</TableCell>
+                  <TableCell>{row.number}</TableCell>
+                  <TableCell>{row.group}</TableCell>
                   <TableCell>{row.date}</TableCell>
                   <TableCell>{date.getHours.toString()}</TableCell>
                   <TableCell align="right">
@@ -124,7 +146,7 @@ export default function TableQ(props) {
                       color="primary"
                       style={{ borderRadius: 5 }}
                       onClick={() =>
-                        handlePaid("Notification sent to " + row.name)
+                        handlePaid(row)
                       }
                     >
                       <span style={{ color: "white" }}>Notify</span>
