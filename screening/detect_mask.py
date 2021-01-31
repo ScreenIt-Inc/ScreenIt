3afc010f49724691ad46036a50cca7a0cad66480
gurnain <gurnaindeepsingh@gmail.com>
2021-01-31 12:27:24 -0500

    refactored thermal screener into class

--- screening/detect_mask.py
+++ screening/detect_mask.py
@@ -1,3 +1,7 @@
+# USAGE
+# python detect_mask_video.py
+
+# import the necessary packages
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
