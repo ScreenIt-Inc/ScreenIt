4e4fae8cb73c2bd526249e5c47d54e3663614630
gurnain <gurnaindeepsingh@gmail.com>
2021-01-31 17:59:14 -0500

    changed output to .png

--- screening/generateQR.py
+++ screening/generateQR.py
@@ -1,5 +1,6 @@
 import pyqrcode
 import requests
+import pygame
 FORM_ENDPOINT = '/customerform'
 UUID_ENDPOINT = '/api/form/opennewformuuid'
 
@@ -7,32 +8,33 @@ UUID_ENDPOINT = '/api/form/opennewformuuid'
 Argument INFO
 
 siteBaseUrl:
-	wheres the site
+    wheres the site
 
 error:
-	Error level   |   % of code can be corrected
-		L 					7%
-		M 					15%
-		Q 					25% 			(most common choice)
-		H 					30%
+    Error level   |   % of code can be corrected
+        L                   7%
+        M                   15%
+        Q                   25%             (most common choice)
+        H                   30%
 
-	increasing level increases image required resolution
+    increasing level increases image required resolution
 
 save_Location:
-	image save location
+    image save location
 
 scale:
-	The scale parameter sets how large to draw a single module.
-	By default one point (1/72 inch) is used to draw a single module.
-	This may make the code to small to be read efficiently.
-	Increasing the scale will make the code larger.
-	This method will accept fractional scales (e.g. 2.5).
+    The scale parameter sets how large to draw a single module.
+    By default one point (1/72 inch) is used to draw a single module.
+    This may make the code to small to be read efficiently.
+    Increasing the scale will make the code larger.
+    This method will accept fractional scales (e.g. 2.5).
 '''
 #saves to url
-def generateQR(siteBaseUrl, temp, error='Q', save_Location='url.svg', scale=8):
-	r = requests.get(url=siteBaseUrl+UUID_ENDPOINT+'/'+str(temp))
-	uuid_data = r.json()
-	if uuid_data['success']:
-		url = pyqrcode.create(siteBaseUrl + FORM_ENDPOINT + '/' + uuid_data['data']['uuid'], error=error)
-		url.svg(save_Location, scale=scale)
-	return 0
+def generateQR(siteBaseUrl, temp, error='Q', save_Location='url.png', scale=8):
+    url_str = siteBaseUrl + UUID_ENDPOINT + '/' + str(temp)
+    r = requests.get(url=url_str)
+    uuid_data = r.json()
+    if uuid_data['success']:
+        url = pyqrcode.create(siteBaseUrl + FORM_ENDPOINT + '/' + uuid_data['data']['uuid'], error=error)
+        url.png(save_Location, scale=scale)
+    return url_str
